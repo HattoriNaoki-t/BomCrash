70bfbfd1ecda3644af2ebd5307c466bc74824714
HattoriNaoki <toraidennto1323@gmail.com>
2017-10-12 15:51:53 +0900

    基盤

---
+++ BombCrash/Assets/CharMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+
+
+    }
+}
---
+++ BombCrash/Assets/StageCreate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour {
+
+    public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                var chil  = Instantiate(floor, new Vector3(i, 0, j), Quaternion.identity);
+                chil.transform.parent = gameObject.transform;
+                chil.name = i.ToString() + j.ToString();
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
