a0fcaaf6efccde5bd7c63f6bff79d7934dbcd2f5
Mrmshr <1205mshr@gmail.com>
2017-11-22 16:25:37 +0900

    slot Effect

---
+++ slotEffect/Assets/Title/TitleScene2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //押されたら
+		if(Input.GetKeyDown(KeyCode.Joystick1Button5))
+        {
+            //シーン移行
+            SceneManager.LoadScene("surotto");
+            Debug.Log("ゲームスタート");
+        }
+	}
+}
